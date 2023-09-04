259a8daf6e9f05c85d294a7869faf674977b4741
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-09-04 08:46:42 +0200

    unity update for simple api call

--- php Unity/Assets/DebugRequestGUI.cs
+++ php Unity/Assets/DebugRequestGUI.cs
@@ -1,21 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UIElements;
 
 public class DebugRequestGUI : MonoBehaviour
 {
-    private VisualElement root;
+    string url = "http://127.0.0.1/edsa-asyncserver/";
 
+    private VisualElement root;
     private Button requestButton;
+    private Label debugLabel;
 
     private void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         requestButton = root.Q<Button>("Send-Button");
+        debugLabel = root.Q<Label>("Output-Label");
+        debugLabel.text = "";
         requestButton.RegisterCallback<ClickEvent>(evt =>
         {
-            Debug.Log("De knop werkt");
+            StartCoroutine(RandomFruit());
         });
     }
+
+
+
+    //I want to commit
+
+    IEnumerator RandomFruit()
+    {
+
+        NameList request = new NameList();
+
+
+        string json = JsonUtility.ToJson(request);
+        Debug.Log(json);
+
+        List<IMultipartFormSection> formData = new();
+        MultipartFormDataSection entery = new MultipartFormDataSection("json", json);
+
+        formData.Add(entery);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, formData))
+        {
+            yield return webRequest.SendWebRequest();
+            Debug.Log(webRequest.downloadHandler.text);
+            FruitResponse response = JsonUtility.FromJson<FruitResponse>(webRequest.downloadHandler.text);
+            debugLabel.text += response.fruit[Random.Range(0, response.fruit.Length)] + "\n";
+        }
+    }
+}
+
+
+public class NameList
+{
+    public string action = "get_fruit";
+}
+
+public class FruitResponse
+{
+    public string[] fruit;
 }
--- php Unity/Assets/fruit.cs
+++ php Unity/Assets/fruit.cs
@@ -1,46 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
 using UnityEngine.Networking;
 
 
 public class fruit : MonoBehaviour
 {
-    string url = "http://127.0.0.1/edsa-asyncserver/";
-
-    //I want to commit
-
-    IEnumerator Start()
-    {
-
-        NameList request = new NameList();
-
-
-        string json = JsonUtility.ToJson(request);
-        Debug.Log(json);
-
-        List<IMultipartFormSection> formData = new();
-        MultipartFormDataSection entery = new MultipartFormDataSection("json", json);
-
-        formData.Add(entery);
-
-        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, formData))
-        {
-            yield return webRequest.SendWebRequest();
-            Debug.Log(webRequest.downloadHandler.text);
-            FruitResponse response = JsonUtility.FromJson<FruitResponse>(webRequest.downloadHandler.text);
-            Debug.Log(response.fruit[Random.Range(0, response.fruit.Length)]);
-        }
-    }
-}
-
-
-public class NameList
-{
-    public string action = "get_fruit";
-}
-
-public class FruitResponse
-{
-    public string[] fruit;
 }
