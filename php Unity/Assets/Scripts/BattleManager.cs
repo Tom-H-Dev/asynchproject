ec19eae8df821a610f44d3fdd0566035ba1b5e83
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-10-06 10:24:16 +0200

    script folder start battle system

---
+++ php Unity/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    //Find new opponent (Goes through the database and find another player by ID that you haven't found yet (stored in list) and display 10% of resources that you can steal)
+    //Pop up of when you are attacked ((new bool in database) and show how many troops and resources you have lost(and remove them from database))
+    //Battle button where you attack the other player
+    //Get a percentage of the enemy troops and display those
+    //display the name of the opponent by id
+    // when you attack play small simple animation to show that you are attacking
+    //check your troops vs enemy troops and check who wins.
+    //Set attack and defense values and combine those when attacking and check who will win.
+}
